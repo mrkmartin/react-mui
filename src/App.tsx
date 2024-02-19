055d19272827cdd6e03241ba36c70540e488cf09
Mark Paulo Martin <markpaulomartin@gmail.com>
2024-02-19 23:24:21 +0800

    Add Rating

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import "./App.css";
+import './App.css';
 // import { MuiTypography } from './components/MuiTypography';
 // import { MuiButton } from './components/MuiButton';
 // import { MuiTextField } from './components/MuiTextField';
 // import { MuiSelect } from './components/MuiSelect';
 // import { MuiRadioButton } from './components/MuiRadioButton';
 // import { MuiCheckbox } from './components/MuiCheckbox';
-import { MuiSwitch } from "./components/MuiSwitch";
+// import { MuiSwitch } from "./components/MuiSwitch";
+import { MuiRating } from './components/MuiRating';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       {/* <MuiTypography></MuiTypography> */}
       {/* <MuiButton></MuiButton> */}
       {/* <MuiTextField></MuiTextField> */}
       {/* <MuiSelect></MuiSelect> */}
       {/* <MuiRadioButton></MuiRadioButton> */}
       {/* <MuiCheckbox></MuiCheckbox> */}
-      <MuiSwitch></MuiSwitch>
+      {/* <MuiSwitch></MuiSwitch> */}
+      <MuiRating></MuiRating>
     </div>
   );
 }

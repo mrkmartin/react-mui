594b85a0a5df15c4e7c25e9e8383b0bfc008eb75
Mark Paulo Martin <markpaulomartin@gmail.com>
2024-02-20 21:58:57 +0800

    Add Autocomplete

--- src/components/MuiAutocomplete.tsx
+++ src/components/MuiAutocomplete.tsx
@@ -1,13 +1,40 @@
-import { Stack, Autocomplete, TextField } from "@mui/material";
+import { Stack, Autocomplete, TextField } from '@mui/material';
+import { useState } from 'react';
 
-const skills = ["HTML", "CSS", "Javascript", "Typescript", "React"];
+type Skill = {
+  id: number;
+  label: string;
+};
+
+const skills = ['HTML', 'CSS', 'Javascript', 'Typescript', 'React'];
+
+const skillsOptions = skills.map((skill, index) => ({
+  id: index + 1,
+  label: skill,
+}));
 
 export const MuiAutocomplete = () => {
+  const [value, setValue] = useState<string | null>(null);
+  const [skill, setSkill] = useState<Skill | null>(null);
+  console.log({ skill });
   return (
-    <Stack spacing={2} width="250px">
+    <Stack spacing={2} width='250px'>
       <Autocomplete
         options={skills}
-        renderInput={(params) => <TextField></TextField>}
+        renderInput={(params) => (
+          <TextField {...params} label='Skills'></TextField>
+        )}
+        value={value}
+        onChange={(event: any, newValue: string | null) => setValue(newValue)}
+        freeSolo
+      ></Autocomplete>
+      <Autocomplete
+        options={skillsOptions}
+        renderInput={(params) => (
+          <TextField {...params} label='Skills'></TextField>
+        )}
+        value={skill}
+        onChange={(event: any, newValue: Skill | null) => setSkill(newValue)}
       ></Autocomplete>
     </Stack>
   );

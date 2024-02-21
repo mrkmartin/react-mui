19012124906e39d559df8c167dcddeec454f40d5
Mark Paulo Martin <jetblackzero04@gmail.com>
2024-02-21 19:36:50 +0800

    Add Stack component

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,29 +1,34 @@
-import { Box } from '@mui/material';
+import { Box, Stack, Divider } from "@mui/material";
 
 export const MuiLayout = () => {
   return (
-    <>
+    <Stack
+      sx={{ border: "1px solid" }}
+      direction="row"
+      spacing={2}
+      divider={<Divider orientation="vertical" flexItem></Divider>}
+    >
       <Box
         sx={{
-          backgroundColor: 'primary.main',
-          color: 'white',
-          height: '100px',
-          width: '100px',
-          padding: '16px',
-          '&:hover': {
-            backgroundColor: 'primary.light',
+          backgroundColor: "primary.main",
+          color: "white",
+          height: "100px",
+          width: "100px",
+          padding: "16px",
+          "&:hover": {
+            backgroundColor: "primary.light",
           },
         }}
       >
         Material UI
       </Box>
       <Box
-        display='flex'
-        height='100px'
-        width='100px'
-        bgcolor='success.light'
+        display="flex"
+        height="100px"
+        width="100px"
+        bgcolor="success.light"
         p={2}
       ></Box>
-    </>
+    </Stack>
   );
 };

1a4ff7ad2bee9a8570cecec4cc64113b635d6c5d
Mark Paulo Martin <markpaulomartin@gmail.com>
2024-02-23 21:06:57 +0800

    Add Link component

--- src/components/MuiLink.tsx
+++ src/components/MuiLink.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Stack, Link, Typography } from '@mui/material';
 
 export const MuiLink = () => {
@@ -12,15 +11,6 @@ export const MuiLink = () => {
           Secondary
         </Link>
       </Typography>
-=======
-import { Stack, Link } from "@mui/material";
-
-export const MuiLink = () => {
-  return (
-    <Stack spacing={2} direction="row" m={4}>
-      <Link href="#">Link</Link>
-      <Link href="#">Link</Link>
->>>>>>> c9eea9e88a14d9576856969141ebf23ac2e7105d
     </Stack>
   );
 };
